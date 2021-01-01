a544ad83dcb58db9967ee6d1acd9dbf7c1acaec4
amar454 <aaamargml@gmail.com>
2020-12-31 21:20:36 -0800

    debugged mkfile, others

--- msys/lib/cat.py
+++ msys/lib/cat.py
@@ -1,5 +1,13 @@
-def cat(commandList, rootDir):
 
+
+def cat(commandList, rootDir):
+    validOptions = {'-l', }
+    LINES_COMMANDS = False
+    optionList = []
+    errorOption = None
+    if len(commandList) == 1:
+        return print('cat: no input files')
+    if len(commandList) == 2:
         try:
             with open(commandList[1].replace('~', rootDir)) as f:
                 print(f.read())
@@ -7,3 +15,23 @@ def cat(commandList, rootDir):
 
         except OSError:
             return print(f'cat: {commandList[1]}: not a valid file or file path')
+    for option in commandList:
+
+        if option in validOptions:
+            if option == '-l':
+                if not LINES_COMMANDS:
+                    LINES_COMMANDS = True
+                    optionList.append(option)
+                else:
+                    return print()
+        else:
+            optionList.append(option)
+            errorOption = option
+            break
+    try:
+        with open(errorOption.replace('~', rootDir)) as f:
+            print(f.read())
+            f.close()
+
+    except OSError:
+        return print(f'cat: {commandList[1]}: not a valid file or file path')
--- msys/lib/mkfile.py
+++ msys/lib/mkfile.py
@@ -1,14 +1,12 @@
+import os
+
+
 def mkfile(commandList, rootDir):
     if len(commandList) == 1:
         return None
     elif len(commandList) > 1:
-        try:
-            with open(commandList[1].replace('~', rootDir), 'w+') as f:
-                return None
-        except FileExistsError:
-            return print('%s: %s: file already exists in directory; try another name' % (commandList[0], commandList[1]))
-
-
-
-
-
+        if os.path.exists(commandList[1]):
+            return print(
+                '%s: %s: file already exists in directory; try another name' % (commandList[0], commandList[1]))
+        with open(commandList[1].replace('~', rootDir), 'w+') as f:
+            return None
